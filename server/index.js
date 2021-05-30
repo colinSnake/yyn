67904f73265aa350086480dc6f0cd3275d0597a5
Magic <316063522@qq.com>
2021-05-30 17:29:57 +0800

    chore: Build the project structure.

---
+++ server/index.js
@@ -0,0 +1,9 @@
+/*
+ * @,@Descripttion: ,: 
+ * @,@version: ,: 
+ * @,@Author: ,: Magic
+ * @,@Date: ,: 2021-05-30 17:21:50
+ * @,@LastEditors: ,: Magic
+ * @,@LastEditTime: ,: 2021-05-30 17:22:07
+ */
+const path = require('path');
